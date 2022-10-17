9b266f5cb33a67434610406cbff85a52ade22bef
Olya Krugova <j-olga.k@yandex.ru>
2022-10-17 13:03:16 +0300

    последние правки

--- js/main.js
+++ js/main.js
@@ -19,11 +19,27 @@ $(function(){
             start: "top top", // when the top of the trigger hits the top of the viewport
             end: "+=500", // end after scrolling 500px beyond the start
             scrub: 0.5, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-            marcers: true
+            // markers: true
+            onUpdate: () => {
+                setTimeout(() => {
+                    $(".about-block").css("padding-top", getHeaderHeight() + "px")
+                }, 0);
+            }
         }, // start the animation when ".box" enters the viewport (once)
-        height: 130,
-        backgroundSize: "110%"
+        height: () => {
+            return 130 * 100 / 1920 + "vw";
+        },
     });
     $(window).trigger("resize");
+    gsap.to(".header-background img", {
+        scrollTrigger: {
+            trigger: ".about-block",
+            start: "top top", // when the top of the trigger hits the top of the viewport
+            end: "+=500", // end after scrolling 500px beyond the start
+            scrub: 0.5, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+            marcers: true
+        }, // start the animation when ".box" enters the viewport (once)
+        scale: 1.1
+    });
 
 });
